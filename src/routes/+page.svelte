b88ba111e6eb0ad9338d00711b009e9de9f763e2
ChillarAnand <chillar@avilpage.com>
2024-12-31 20:20:36 +0530

    Add nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+
+    import Nav from "./Nav.svelte";
+
+</script>
+
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    />
+</svelte:head>
+
+<Nav />
